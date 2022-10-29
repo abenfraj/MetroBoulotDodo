9c5c384613445bad11400d6fb8615a7b6fa6b7ee
abenfraj <72936702+abenfraj@users.noreply.github.com>
2022-10-29 19:03:49 +0200

    indent

--- backend/helpers/kruskal.js
+++ backend/helpers/kruskal.js
@@ -1,70 +1,70 @@
-const { parcoursProfondeur } = require("./connexe");
 const { Graphe } = require("./graphe");
 
 const find = (parent, i) => {
-    if (parent[i] == i) {
-        return i;
-    }
-    return find(parent, parent[i]);
+  if (parent[i] == i) {
+    return i;
+  }
+  return find(parent, parent[i]);
 };
 
 const union = (parent, rank, x, y) => {
-    const xroot = find(parent, x);
-    const yroot = find(parent, y);
-    if (rank[xroot] < rank[yroot]) {
-        parent[xroot] = yroot;
-    }
-    else if (rank[xroot] > rank[yroot]) {
-        parent[yroot] = xroot;
-    }
-    else {
-        parent[yroot] = xroot;
-        rank[xroot] += 1;
-    }
+  const xroot = find(parent, x);
+  const yroot = find(parent, y);
+  if (rank[xroot] < rank[yroot]) {
+    parent[xroot] = yroot;
+  } else if (rank[xroot] > rank[yroot]) {
+    parent[yroot] = xroot;
+  } else {
+    parent[yroot] = xroot;
+    rank[xroot] += 1;
+  }
 };
 
-const supprimerDoublons = (sommets) => {
-    const sommetsUniques = [];
-    sommets.forEach((sommet) => {
-        if (!sommetsUniques.some((sommetUnique) => sommetUnique.nomSommet === sommet.nomSommet)) {
-            sommetsUniques.push(sommet);
-        }
-    });
-    return sommetsUniques;
+const supprimerDoublonsSommets = (sommets) => {
+  const sommetsUniques = [];
+  sommets.forEach((sommet) => {
+    if (
+      !sommetsUniques.some(
+        (sommetUnique) => sommetUnique.nomSommet === sommet.nomSommet
+      )
+    ) {
+      sommetsUniques.push(sommet);
+    }
+  });
+  return sommetsUniques;
 };
 
 const kruskal = (graphe) => {
-    const sommets = supprimerDoublons(graphe.sommets);
-    const arcs = graphe.arcs;
-    const arbre = [];
-    let i = 0;
-    let e = 0;
-    arcs.sort((a, b) => a.tempsEnSecondes - b.tempsEnSecondes);
-    const parent = [];
-    const rank = [];
-    for (i = 0; i < sommets.length; i++) {
-        parent[i] = i;
-        rank[i] = 0;
+  const sommets = graphe.sommets;
+  const arcs = graphe.arcs;
+  const parent = [];
+  const rank = [];
+  const mst = [];
+  let i = 0;
+  let e = 0;
+  arcs.sort((a, b) => a.tempsEnSecondes - b.tempsEnSecondes);
+  sommets.forEach((sommet) => {
+    parent.push(sommet.numSommet);
+    rank.push(0);
+  });
+  while (e < sommets.length - 1) {
+    const arc = arcs[i++];
+    const x = find(parent, arc.numSommet1);
+    const y = find(parent, arc.numSommet2);
+    if (x != y) {
+      e++;
+      mst.push(arc);
+      union(parent, rank, x, y);
     }
-    i = 0;
-    while (e < sommets.length - 1) {
-        const arc = arcs[i++];
-        const x = find(parent, arc.numSommet1);
-        const y = find(parent, arc.numSommet2);
-        if (x != y) {
-            e++;
-            arbre.push(arc);
-            union(parent, rank, x, y);
-        }
-    }
-    let cpt = 0;
-    for (let i = 0; i < arbre.length; i++) {
-        cpt += arbre[i].tempsEnSecondes;
-    }
-    console.log("Temps total de l'arbre: " + cpt);
-    return arbre;
+  }
+  let cpt = 0;
+  mst.forEach((arc) => {
+    cpt += arc.tempsEnSecondes;
+  });
+  console.log("mst", cpt);
+  return mst;
 };
 
 module.exports = {
-    kruskal,
+  kruskal,
 };
